0d40d9fe154b687e240ce0f8ca0263ebecde7c46
nestor.huallpa <huallpa.nestor@gmail.com>
2022-10-06 00:07:07 -0300

    Test and fuction

---
+++ src/my_peg_thing/core.clj
@@ -0,0 +1,119 @@
+(ns my-peg-thing.core
+  (:require [clojure.set :as set])
+  (:gen-class))
+
+(declare succesful-move promt-move game-over query-rows)
+
+
+(defn tri*
+  "Generates lazy sequence of triangular numbers"
+  ([] (tri* 0 1))
+  ([sum n]
+    (let [new-sum (+ sum n)] 
+      (cons new-sum (lazy-seq (tri* new-sum (inc n)))))))
+
+(def tri (tri*))
+
+(defn triangular?
+  "Is the number triangular? e.g. 1, 3, 6, 10, etc"
+  [n]
+  (= n (last (take-while #(>= n %) tri))))
+
+(defn row-tri
+  "The triangular number at the end of row n"
+  [n]
+  (last (take n tri)))
+
+(defn row-num
+  "Returns row number the position belongs to: pos 1 in row 1,
+  positions 2 and 3 in row 2, etc"
+  [pos]
+  (inc (count (take-while #(> pos %) tri))))
+
+(defn connect
+  "Form a mutual connection between two positions"
+  [board max-pos pos neighbor destination]
+  (if (<= destination max-pos)
+    (reduce (fn [new-board [p1 p2]]
+              (assoc-in new-board [p1 :connections p2] neighbor))
+            board
+            [[pos destination] [destination pos]])
+    board))
+
+(defn connect-down-left
+  [board max-pos pos]
+  (let [row (row-num pos)
+        neighbor (+ row pos)
+        destination (+ 1 row neighbor)]
+    (connect board max-pos pos neighbor destination)))
+
+(defn connect-down-right
+  [board max-pos pos]
+  (let [row (row-num pos)
+        neighbor (+ 1 row pos)
+        destination (+ 2 row neighbor)]
+    (connect board max-pos pos neighbor destination)))
+
+(defn connect-right
+  [board max-pos pos]
+  (let [neighbor (inc pos)
+        destination (inc neighbor)]
+    (if-not (or (triangular? neighbor) (triangular? pos))
+      (connect board max-pos pos neighbor destination)
+      board)))
+
+(defn add-pos
+  [board max-pos pos]
+  (let [pegged-board (assoc-in board [pos :pegged] true)]
+    (reduce (fn [new-board connection-creation-fn]
+              (connection-creation-fn new-board max-pos pos))
+            pegged-board
+            [connect-right connect-down-left connect-down-right])))
+
+(defn new-board
+  [rows]
+  (let [initial-board {:rows rows}
+        max-pos (row-tri rows)]
+    (reduce (fn [board pos] (add-pos board max-pos pos))
+            initial-board
+            (range 1 (inc max-pos)))))
+
+(defn pegged?
+  "Does the position have a peg in it?"
+  [board pos]
+  (get-in board [pos :pegged]))
+
+(defn remove-peg
+  [board pos]
+  (assoc-in board [pos :pegged] false))
+
+(defn place-peg
+  [board pos]
+  (assoc-in board [pos :pegged] true))
+
+(defn move-peg
+  [board p1 p2]
+  (place-peg (remove-peg board p1) p2))
+
+(defn valid-moves
+  "Return a map of all valid moves for pos, where the key is the destination and the value is the jumped position"
+  [board pos]
+  (into {}
+        (filter (fn [[destination jumped]]
+                  (and (not (pegged? board destination))
+                       (pegged? board jumped)))
+                (get-in board [pos :connections]))))
+
+(defn valid-move?
+  [board p1 p2]
+  (get (valid-moves board p1) p2))
+
+(defn make-move
+  [board p1 p2]
+  (if-let [jumped (valid-move? board p1 p2)]
+    (move-peg (remove-peg board jumped) p1 p2)))
+
+(defn -main
+  "I don't do a whole lot ... yet."
+  [& args]
+  (println "Hello, World!"))
---
+++ test/my_peg_thing/core_test.clj
@@ -0,0 +1,165 @@
+(ns my-peg-thing.core-test
+  (:require [clojure.test :refer :all]
+            [my-peg-thing.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 1 1))))
+
+(deftest tri-test
+  (testing "Tri function"
+    (is (= (take 2 tri) '(1 3)))))
+
+(deftest triangular?-test
+  (testing "Validate 5 is not a triangular number"
+    (is (= (triangular? 5) false)))
+  (testing "Validate 6 is a triangular number"
+    (is (= (triangular? 6) true))))
+
+(deftest test-row-tri
+  (testing "Get the triangular number at the end of row n"
+    (are [x y] (= (row-tri x) y)
+          1 1
+          2 3
+          3 6)))
+
+(deftest test-row-number
+  (testing "Returns row number"
+    (are [x y] (= (row-num x) y)
+          1 1
+          5 3)))
+
+(def connections1and4 
+  {1 {:connections {4 2}}
+   4 {:connections {1 2}}})
+
+(deftest test-connect
+  (testing "Form a mutual connection between two positions"
+    (is (= (connect {} 15 1 2 4) connections1and4))))
+
+(def connection-for-1
+  {1 {:connections {4 2}}
+   4 {:connections {1 2}}})
+
+(def connection-for-3
+  {3 {:connections {10 6}}
+   10 {:connections {3 6}}})
+
+(def connection-for-4
+  {4 {:connections {6 5}}
+   6 {:connections {4 5}}})
+
+(deftest test-connect-down-left
+  (testing "Connect position to destination down left"
+    (is (= (connect-down-left {} 15 1) connection-for-1))))
+
+(deftest test-connect-down-right
+  (testing "Connect position to destination down right"
+    (is (= (connect-down-right {} 15 3) connection-for-3))))
+
+(deftest test-connect-right
+  (testing "Connect position"
+    (is (= (connect-right {} 15 4) connection-for-4))))
+
+(def board-pos1
+  { 1 {:connections {6 3, 4 2}, :pegged true}
+    4 {:connections {1 2}}
+    6 {:connections {1 3}}})
+
+(def board-pos4
+  { 4 {:connections {13 8, 11 7, 6 5}, :pegged true}
+    13 {:connections {4 8}}
+    11 {:connections {4 7}}
+    6 {:connections {4 5}}})
+
+(def board-pos6
+  { 6 {:connections {15 10, 13 9}, :pegged true}
+   15 {:connections {6 10}}
+   13 {:connections {6 9}}})
+
+(deftest test-add-pos
+  (testing "Add peg in a position"
+    (is (= (add-pos {} 15 1) board-pos1))
+    (is (= (add-pos {} 15 4) board-pos4))
+    (is (= (add-pos {} 15 6) board-pos6))))
+
+
+(def board {1  {:pegged true, :connections {6 3, 4 2}},
+            2  {:pegged true, :connections {9 5, 7 4}},
+            3  {:pegged true, :connections {10 6, 8 5}},
+            4  {:pegged true, :connections {13 8, 11 7, 6 5, 1 2}},
+            5  {:pegged true, :connections {14 9, 12 8}},
+            6  {:pegged true, :connections {15 10, 13 9, 4 5, 1 3}},
+            7  {:pegged true, :connections {9 8, 2 4}},
+            8  {:pegged true, :connections {10 9, 3 5}},
+            9  {:pegged true, :connections {7 8, 2 5}},
+            10 {:pegged true, :connections {8 9, 3 6}},
+            11 {:pegged true, :connections {13 12, 4 7}},
+            12 {:pegged true, :connections {14 13, 5 8}},
+            13 {:pegged true, :connections {15 14, 11 12, 6 9, 4 8}},
+            14 {:pegged true, :connections {12 13, 5 9}},
+            15 {:pegged true, :connections {13 14, 6 10}},
+            :rows 5})
+
+(deftest test-new-board
+  (testing "Create a board"
+    (is (= (new-board 5) board))))
+
+(deftest test-pegged?
+  (testing "Is pegged?"
+    (is (pegged? board 1))))
+
+(deftest test-remove-peg
+  (testing "remove a peg"
+    (let [board-removed (remove-peg board 1)]
+      (is (not (pegged? board-removed 1)))
+      (is (pegged? board-removed 2)))))
+
+
+(deftest test-place-peg
+  (testing "Place a peg"
+    (let [board-removed (remove-peg board 1)
+          board-placed (place-peg board-removed 1)]
+      (is (pegged? board-placed 1)))))
+
+
+(deftest test-move-peg
+  (testing "move-peg"
+    (let [board-moved (move-peg board 1 3)]
+      (is (pegged? board-moved 3))
+      (is (not (pegged? board-moved 1))))))
+
+
+
+(def board-valid-for-13 {1  {:pegged true, :connections {6 3, 4 2}},
+            2  {:pegged true, :connections {9 5, 7 4}},
+            3  {:pegged true, :connections {10 6, 8 5}},
+            4  {:pegged true, :connections {13 8, 11 7, 6 5, 1 2}},
+            5  {:pegged true, :connections {14 9, 12 8}},
+            6  {:pegged false, :connections {15 10, 13 9, 4 5, 1 3}},
+            7  {:pegged true, :connections {9 8, 2 4}},
+            8  {:pegged true, :connections {10 9, 3 5}},
+            9  {:pegged true, :connections {7 8, 2 5}},
+            10 {:pegged true, :connections {8 9, 3 6}},
+            11 {:pegged false, :connections {13 12, 4 7}},
+            12 {:pegged true, :connections {14 13, 5 8}},
+            13 {:pegged true, :connections {15 14, 11 12, 6 9, 4 8}},
+            14 {:pegged true, :connections {12 13, 5 9}},
+            15 {:pegged false, :connections {13 14, 6 10}},
+            :rows 5})
+
+(deftest test-valid-moves
+  (let [moves (valid-moves board-valid-for-13 13)]
+    (is (= moves {15 14, 11 12, 6 9}))))
+
+(deftest test-valid-move?
+  (is (= (valid-move? board-valid-for-13 13 11) 12))
+  (is (= (valid-move? board-valid-for-13 13 10) nil)))
+
+(deftest test-make-move
+  (testing "test to make move from 6 to 4, jumped 5"
+    (let [my-board (assoc-in (new-board 5) [4 :pegged] false)
+          board-moved (make-move my-board 6 4)]
+      (is (not (pegged? board-moved 5)))
+      (is (not (pegged? board-moved 6)))
+      (is (pegged? board-moved 4)))))
